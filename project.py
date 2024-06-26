786817fc260c6eeb9f23d30bd8fa884f234fcadf
GalTrash <luluisv7@gmail.com>
2024-06-26 20:18:34 -0300

    fiz a data de postagem do video no youtube, fiz tambem o nome do canal e fiz uma nova função para saber se o conteudo é family friend ou não

--- project.py
+++ project.py
@@ -6,15 +6,23 @@ from bs4 import BeautifulSoup
 
 
 def main():
-    url = verify( input("Put the link here: ").strip())
+    url = verify(input("Put the link here: ").strip())
     if(url == False):
         print("Não é um link valido")
     else:
         page = requests.get(url)
-        html_content = page.content
+        html_content = page.content.decode('utf-8')
         soup = BeautifulSoup(html_content, "html.parser")
         title(soup)
         description(soup)
+        author(soup)
+        date(soup)
+
+        if(isfamily(soup) == "true"):
+            print("This video is Family Friend")
+        else:
+            print("This video is not Family Friend")
+        
 #supported links
 #https://www.youtube.com/watch?v=iQeBYPJWtak
 #http://youtu.be/cCnrX1w5luM
@@ -29,10 +37,11 @@ def main():
 #https://www.youtube.com/watch?v=dQw4w9WgXcQ&feature=youtu.be
 #https://www.youtube.com/watch?v=dQw4w9WgXcQ&list=PL9tY0BWXOZFtAk_vlObsc1kYM3Imi2F98&index=3
 #https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley
+#https://youtu.be/ofmSXnoS7FE?si=FPIUjUDnEqJBzEe4
 
 
 def verify(s):
-    if re.search(r"^(https?:)?(\/\/)?(www\.)?(youtube(\.com)?|youtu.be)(\/)((watch\?v=)?\w+)(&\w+=\S+)?((embed)?\/\w+)?(#\S+)?$",s):
+    if re.search(r"^(https?:)?(\/\/)?(www\.)?(youtube(\.com)?|youtu.be)(\/)((watch\?v=)?(\w+\??\w+=\w+)?)(&\w+=\S+)?((embed)?\/\w+)?(#\S+)?$",s):
         return s
     else:
         return False 
@@ -42,16 +51,25 @@ def title(t,):
     print(title_tag["content"])
 
 def author(a):
-    ...
+    container = a.find("div")
+    author_tag = container.find("link", itemprop="name")
+    print(author_tag["content"])
+
 
 def description(d):
     des_tag = d.find("meta", itemprop="description")
     print(des_tag["content"])
 
 def date(dat):
-    ...
+    date_tag = dat.find("meta", itemprop="datePublished")
+    print(date_tag["content"])
+
+def isfamily(f):
+    tag_family = f.find("meta", itemprop="isFamilyFriendly")
+    return tag_family["content"]
 
 if __name__=="__main__":
     main()
 
-#fazer o autor e a data de postagem do video
+#fazer o autor do video
+#fazer o regex pra comportar o novo link de youtube que eu achei https://www.youtube.com/watch?v=hv-hbublepc
